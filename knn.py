01c06065d52d907e852bf2182997a5f940d86f33
maangelicat <maria.mateust@gmail.com>
2024-05-10 19:23:33 -0500

    first commit

---
+++ knn.py
@@ -0,0 +1,8 @@
+peso = [55, 58, 60, 65, 70, 68, 75, 75, 80, 90]
+altura = [160, 165, 160, 166, 168, 171, 175, 180, 187, 190]
+talla = ['S', 'S', 'M', 'M', 'M', 'M', 'L', 'L', 'L', 'L']
+
+x = int (input ("Por favor ingrese el valor del peso"))
+y = int (input ("Por favor ingrese el valor del altura"))
+
+
